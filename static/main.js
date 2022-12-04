bb3164d263e59887b352b07bfbba37e7f735f57f
kirjavascript <snkenjoi@gmail.com>
2022-12-04 23:36:11 +0000

    gamepad stuff

--- static/main.js
+++ static/main.js
@@ -81,7 +81,7 @@ function render(shouldUpdate) {
             arrowsB.forEach((node) => {
                 node.textContent = !typeA ? node.dataset.glyph : ' ';
             });
-            mode.textContent = gameType === 'A' ? 1 : 2;
+            modeEl.textContent = gameType === 'A' ? 1 : 2;
         } else if (menuMode === 'LevelSelect') {
             const selectedLevel = level + (((input & 0x80) / 12) | 0);
             selectedLevelEl.textContent = String(selectedLevel).padStart(2, 0);
@@ -240,6 +240,20 @@ function inputByte() {
     for (bit of controls) {
         byte += controlBytes[bit];
     }
+
+    padmap && Object.entries(padmap).forEach(([index, mapping]) => {
+        if (mapping.length === 2) {
+            const [gamepad, button] = mapping;
+            if (gamepads[gamepad]?.buttons[button].pressed) {
+                byte += controlBytes[index];
+            }
+        } else if (mapping.length === 3) {
+            const [gamepad, axis, direction] = mapping;
+            if (Math.round(gamepads[gamepad]?.axes[axis]) === direction) {
+                byte += controlBytes[index];
+            }
+        }
+    });
     return byte;
 }
 
@@ -292,6 +306,130 @@ html.addEventListener('keyup', (e) => {
         e.preventDefault();
     }
 });
+
+// gamepad
+
+const gamepads = [];
+
+window.addEventListener(
+    'gamepadconnected',
+    (event) => {
+        const gamepad = event.gamepad;
+        gamepads[gamepad.index] = gamepad;
+    },
+    false,
+);
+window.addEventListener(
+    'gamepaddisconnected',
+    (event) => {
+        const gamepad = event.gamepad;
+        delete gamepads[gamepad.index];
+    },
+    false,
+);
+
+// remapping
+
+const buttonNames = ['Up', 'Down', 'Left', 'Right', 'B', 'A', 'Select', 'Start'];
+const pinLookup = [4, 5, 6, 7, 1, 0, 2, 3];
+
+const controlText = document.querySelector('#controls');
+
+document.querySelector('#input').addEventListener('click', () => {
+    remap({
+        setText: (text) => {
+            controlText.textContent = text;
+        },
+        onComplete: () => {
+            controlText.textContent = '';
+        },
+    });
+});
+
+function remap({
+    setText,
+    onComplete,
+}) {
+    const keyRemaps = {};
+    const padRemaps = {};
+
+    let mapIndex = 0;
+    const keydown = (e) => {
+        if (!(e.key in keyRemaps)) {
+            keyRemaps[e.key] = pinLookup[mapIndex];
+            addedMap();
+        }
+    };
+
+    html.addEventListener('keydown', keydown);
+
+    const interval = setInterval(() => {
+        // poll for gamepad presses
+        for (let i = 0; i < gamepads.length; i++) {
+            const gamepad = gamepads[i];
+            const pressed = gamepad.buttons.findIndex((d) => d.pressed);
+            if (pressed !== -1) {
+                const alreadyPressed = Object.values(padRemaps)
+                    .filter((d) => d.length === 2)
+                    .some(([_, alreadyPressed]) => alreadyPressed === pressed);
+                if (!alreadyPressed) {
+                    padRemaps[pinLookup[mapIndex]] = [i, pressed];
+                    addedMap();
+                    break;
+                }
+            } else {
+                const axes = gamepad.axes.findIndex((d) => Math.abs(d) > 0.51);
+                if (axes !== -1) {
+                    const direction = Math.round(gamepad.axes[axes]);
+                    const alreadyTilted = Object.values(padRemaps)
+                        .filter((d) => d.length === 3)
+                        .some(
+                            ([_, alreadyTilted, alreadyDir]) =>
+                            alreadyTilted === axes && alreadyDir === direction,
+                        );
+
+                    if (!alreadyTilted) {
+                        padRemaps[pinLookup[mapIndex]] = [i, axes, direction];
+                        addedMap();
+                        break;
+                    }
+                }
+            }
+        }
+    }, 100);
+
+    const showMessage = () => {
+        setText(`Enter input for ${buttonNames[mapIndex]}`);
+    };
+    showMessage();
+
+    const addedMap = () => {
+        mapIndex++;
+        if (mapIndex === 8) {
+            html.removeEventListener('keydown', keydown);
+            clearInterval(interval);
+
+            controls.clear();
+            keymap = keyRemaps;
+            if (Object.keys(padRemaps)) {
+                padmap = padRemaps;
+            } else {
+                padmap = undefined;
+            }
+
+            window.localStorage.setItem(
+                'controls',
+                JSON.stringify({
+                    keymap,
+                    padmap,
+                }),
+            );
+            onComplete();
+        } else {
+            showMessage();
+        }
+    };
+}
 
 // sfx
 
